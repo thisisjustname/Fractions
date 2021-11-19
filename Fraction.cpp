44757d88315f9aa0c6b867c524c2b3cd96b0dc98
thisisjustname <kovzovstas@gmail.com>
2021-11-19 14:35:53 +0300

    Сreation

---
+++ Fraction.cpp
@@ -0,0 +1,6 @@
+#include "Fraction.h"
+
+Fraction::Fraction(int num, int denum) {
+    numerator = num;
+    denominator = denum;
+}
---
+++ Fraction.h
@@ -0,0 +1,18 @@
+//
+// Created by stuar on 19.11.2021.
+//
+
+#ifndef SAVEME_FRACTION_H
+#define SAVEME_FRACTION_H
+
+
+class Fraction {
+private:
+    int numerator, denominator;
+public:
+    Fraction(int num, int denum);
+
+};
+
+
+#endif //SAVEME_FRACTION_H
